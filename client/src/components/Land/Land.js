9795b8f2e1c64c94bdad5f3d7b9d0c49a14f9520
binders <binderl@debian-BULLSEYE-live-builder-AMD64>
2021-12-20 13:11:58 +0100

    (rebase from main last commit) first commit without migration, config, useless file and fix bug

---
+++ client/src/components/Land/Land.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+
+import PropJson from "../../contracts/MonopolyProp.json";
+import BuildJson from "../../contracts/MonopolyBuild.json";
+import BankJson from "../../contracts/MonopolyBank.json";
+
+export default function Land(props) {
+  const provider = props.eth_provider;
+  const landInfo = props.land_info;
+
+  return (
+    <div>
+      <div>{landInfo.title}</div>
+      <div>Rare price: {landInfo.prices.rare} MONO$</div>
+      <button>Buy</button>
+      <button>Sell</button>
+      <div>Uncommon price: {landInfo.prices.uncommon} MONO$</div>
+      <button>Buy</button>
+      <button>Sell</button>
+      <div>Common price: {landInfo.prices.common} MONO$</div>
+      <button>Buy</button>
+      <button>Sell</button>
+      <div>House price: {landInfo.bprices.house} MONO$</div>
+      <button>Buy</button>
+      <button>Sell</button>
+      <div>Hotel price: {landInfo.bprices.hotel} MONO$</div>
+      <button>Buy</button>
+      <button>Sell</button>
+    </div>
+  );
+}
