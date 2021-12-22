be8b5a6b922f08763db9331e53c8b6b9dde4f863
binders <binderl@debian-BULLSEYE-live-builder-AMD64>
2021-12-22 03:03:16 +0100

    add stMPs staking contract and associated test file

--- test/test_MPs.js
+++ test/test_MPs.js
@@ -2,34 +2,34 @@ const { BN, ether, expectRevert, expectEvent } = require('@openzeppelin/test-hel
 const { expect } = require('chai');
 const { assert } = require('console');
 
-const MPs = artifacts.require('MPs');
+const Rogue = artifacts.require('Rogue');
 
 contract("Mps.sol", function (accounts){
 	const owner = accounts[0];
 	const player1 = accounts[1];
 	const player2 = accounts[2];
 
-	let _MPs;
+	let MPs;
 
 	context("fonctions", function() {
 		const zero_address = "0x0000000000000000000000000000000000000000";    
 		beforeEach(async function () {
-			_MPs = await MPs.new({from:owner});
+			MPs = await Rogue.new({from:owner});
 		});
 		it("... test de la fonction allow", async () => { 
-			let result = await expectRevert(_MPs.allow(player1, player2, {from:player1}),"only owner can call this method");
+			let result = await expectRevert(MPs.allow(player1, player2, {from:player1}),"only owner can call this method");
 		});
 			
 		it("... test de la fonction mint", async () => {
-			await expectRevert(_MPs.mint(owner, 1000000, {from:owner}),"minting is not enable");
+			await expectRevert(MPs.mint(owner, 1000000, {from:owner}),"minting is not enable");
 			let faucet1 = player1;
 			let faucet2 = player2;
-			result = await _MPs.allow(faucet1,faucet2, {from:owner});
+			result = await MPs.allow(faucet1,faucet2, {from:owner});
 
-			let _result = await _MPs.mint(owner, 1000000, {from:faucet1});
+			let _result = await MPs.mint(owner, 1000000, {from:faucet1});
 			expectEvent(_result, 'Transfer', {from: zero_address, to: owner});
 
-			let balance = await _MPs.balanceOf(owner);
+			let balance = await MPs.balanceOf(owner);
 			balance = balance.toNumber();
 			expect(balance).to.equal(1000000, "balance != mint");
 		});
