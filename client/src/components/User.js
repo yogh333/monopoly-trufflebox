e0985fcae6833d03902ae7aafb1f707412a78645
Alex4422 <alexandregautier82@gmail.com>
2021-12-29 17:42:47 +0100

    commit after git rebase

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -2,11 +2,14 @@ import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import Spinner from "react-bootstrap/Spinner"
 
-import MonoJson from "../contracts/MonopolyMono.json";
-import PropJson from "../contracts/MonopolyProp.json";
+import MonoJson from "../../contracts/MonopolyMono.json";
+import BankJson from "../../contracts/MonopolyBank.json";
+import PropJson from "../../contracts/MonopolyProp.json";
+import BoardJson from "../../contracts/MonopolyBoard.json";
+import BuildJson from "../contracts/MonopolyBuild.json";
 
 import "../css/User.css";
-import BuildJson from "../contracts/MonopolyBuild.json";
+import Button from "react-bootstrap/Button";
 
 export default function User(props) {
   const spinner = <Spinner as="span" animation="border" size="sm" />
@@ -15,8 +18,14 @@ export default function User(props) {
   const address = props.address;
   const networkId = props.network_id;
 
+  const [Bank, setBank] = useState(null);
+  const [Board, setBoard] = useState(null);
   const [balance, setBalance] = useState(spinner);
   const [prop, setProp] = useState(spinner);
+  const [rollDice, setRollDice] = useState([2, 4]);
+  const [currentPosition, setCurrentPosition] = useState(0);
+
+
 
   useEffect(() => {
     if (!(provider && address && networkId)) {
@@ -35,14 +44,135 @@ export default function User(props) {
       provider
     );
 
+    const Bank = new ethers.Contract(
+      BankJson.networks[networkId].address,
+      BankJson.abi,
+      provider.getSigner()
+    );
+
+    const Board = new ethers.Contract(
+      BoardJson.networks[networkId].address,
+      BoardJson.abi,
+      provider.getSigner()
+    );
+
+    setBank(Bank);
+    setBoard(Board);
     Mono.balanceOf(address).then((value) => setBalance(ethers.utils.formatUnits(value)));
     Prop.balanceOf(address).then((value) => setProp(value.toNumber()));
   }, [address, provider, networkId]);
+
+  /**
+   * name: rollDiceFunction
+   * description: simulates the roll of dice to move the game forward and to move on the pawn
+   * @returns {Promise<void>}
+   */
+  async function rollDiceFunction() {
+    if (BoardSC == null) return;
+
+    //const getRandomNumber = await BoardSC.getRandomKeccak();
+    //console.log('getRandomNumber(): ', getRandomNumber);
+
+    const getRandomNumber = await BoardSC.getRandomKeccak();
+    console.log('getRandomNumber():!!!!!!!!!! ', getRandomNumber);
+    console.log('hello!');
+
+    //TODO: Replace by the call at the oracle
+    //Generates a random number by JS
+    const generateNewNumber = () => Math.floor(Math.random() * 6 + 1);
+
+    const newValue1 = generateNewNumber();
+    const newValue2 = generateNewNumber();
+
+    console.log({rollDice})
+
+    const total = calculateTotal(newValue1, newValue2)
+    handleNewPosition(currentPosition, total);
+    console.log('total:', {total});
+    setRollDice([newValue1, newValue2]);
+  }
+
+  /**
+   * name: handleNewPosition
+   * description: make all the operations to determine and to display the new position of the future pawn
+   * @param previousPosition
+   * @param total
+   */
+  function handleNewPosition(previousPosition, total){
+    const newCell = previousPosition + total;
+
+    //TODO: to define more accurately
+    if (newCell >= 40) return;
+    highlightCurrentCell(newCell);
+    setCurrentPosition(newCell);
+    forgetPreviousPosition(previousPosition);
+  }
+
+  /**
+   * name: forgetPreviousPosition
+   * description: allows to remove the highlighting of the cell of the previous sum of the dice
+   * @param previousPosition
+   */
+  function forgetPreviousPosition(previousPosition) {
+    document.getElementById(`cell-${previousPosition}`).classList.remove("active");
+  }
+
+  /**
+   * name: highlightCurrentCell
+   * description: highlight the cell which is the result of the sum of the dice
+   * @param total
+   */
+  function highlightCurrentCell(total){
+    const activeCell = document.getElementById(`cell-${total}`);
+    activeCell.classList.add("active");
+  }
+
+  /**
+   * name: calculateTotal
+   * description: calculate the sum of the results of the dice
+   * @param args
+   * @returns {*}
+   */
+  function calculateTotal(...args){
+    console.log({args});
+    //sum the parameters
+    return args.reduce((total, current) => total + current,0);
+  }
 
   return (
     <div>
       <div>{balance} MONO$</div>
       <div>{prop} PROP$</div>
+
+      <Button type='submit' variant="danger" size="sm" className='btn btn-primary btn-lg btn-block'
+              onClick={rollDiceFunction}>
+        Roll the dice!
+      </Button>
+
+
+
+      <div className='mt-3 ml-150'>
+
+        {/* first dice display */}
+        <img
+          className='dice-display'
+          src={require(`../../../build/images/dice_face_${rollDice[0]}.png`).default}
+          alt="dice display"
+
+        />
+      </div>
+
+      <div className='mt-3 ml-150'>
+
+        {/* second dice display */}
+        <img
+          className='dice-display'
+          src={require(`../../../build/images/dice_face_${rollDice[1]}.png`).default}
+          alt="dice display"
+        />
+
+      </div>
+
     </div>
   );
 }
